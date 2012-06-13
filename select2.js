38b9d060b116fc4957110f25b03b8437be981136
Andy Fowler <afowler@nutshell.com>
2012-06-13 18:06:41 -0400

    make getPlaceholder() compatible with jquery 1.4
    (doesn't check data- attrs when using .data())

--- select2.js
+++ select2.js
@@ -809,7 +809,10 @@
         },
 
         getPlaceholder: function () {
-            return this.opts.element.attr("placeholder") || this.opts.element.data("placeholder") || this.opts.placeholder;
+            return this.opts.element.attr("placeholder") ||
+                this.opts.element.attr("data-placeholder") || // jquery 1.4 compat
+                this.opts.element.data("placeholder") ||
+                this.opts.placeholder;
         },
 
         /**
