36e87324db9d79ac611d1ccedc74d4c214bb7dc2
Andy Fowler <afowler@nutshell.com>
2012-06-13 18:09:21 -0400

    adds opts.nativePlaceholder boolean for multiple selects, to assign the placeholder value to the sliding <input> field.

--- select2.js
+++ select2.js
@@ -1117,7 +1117,8 @@
             var opts = this.parent.prepareOpts.apply(this, arguments);
 
             opts = $.extend({}, {
-                closeOnSelect: true
+                closeOnSelect: true,
+                nativePlaceholder: false,
             }, opts);
 
             // TODO validate placeholder is a string if specified
@@ -1212,6 +1213,8 @@
                 this.clearPlaceholder();
             }));
 
+            if (this.opts.nativePlaceholder) this.search.attr('placeholder', this.getPlaceholder());
+
             // set the placeholder if necessary
             this.clearSearch();
         },
@@ -1257,7 +1260,9 @@
                 && this.getVal().length === 0
                 && this.search.hasClass("select2-focused") === false) {
 
-                this.search.val(placeholder).addClass("select2-default");
+                // we're not using the placeholder attribute, just setting the field value directly:
+                if (!this.opts.nativePlaceholder) this.search.val(placeholder).addClass("select2-default");
+
                 // stretch the search box to full width of the container so as much of the placeholder is visible as possible
                 this.search.width(this.getContainerWidth());
             } else {
