43d990c31986094d74ec50de5ec5be18c4d14d88
Igor Vaynberg <igor.vaynberg@gmail.com>
2012-06-15 08:29:47 -0700

    attach the dropdown to the element's body instead of the current body. issue #126

--- select2.js
+++ select2.js
@@ -668,7 +668,7 @@
             if (this.opened()) return;
 
             this.container.addClass("select2-dropdown-open").addClass("select2-container-active");
-            this.dropdown.detach().appendTo('body').addClass("select2-drop-active");
+            this.dropdown.detach().appendTo(this.opts.element.parents("body")).addClass("select2-drop-active");
 
             this.positionDropdown();
 
