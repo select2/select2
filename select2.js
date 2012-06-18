1aad83ea8d790ae58fd63ad6c9f0cb341b445e53
Aziz Gazanchiyan <creage@gmail.com>
2012-06-18 18:08:48 +0300

    Loading next page message formatting implemented, so it could also have localization

--- select2.js
+++ select2.js
@@ -555,6 +555,7 @@
                 },
                 formatNoMatches: function () { return "No matches found"; },
                 formatInputTooShort: function (input, min) { return "Please enter " + (min - input.length) + " more characters"; },
+                formatLoadingNextSet: function (pageNumber) { return "Loading more results..."; },
                 minimumResultsForSearch: 0,
                 minimumInputLength: 0,
                 id: function (e) { return e.id; },
@@ -880,7 +881,7 @@
                 postRender();
 
                 if (data.more === true) {
-                    results.children().filter(":last").append("<li class='select2-more-results'>Loading more results...</li>");
+                    results.children().filter(":last").append("<li class='select2-more-results'>" + opts.formatLoadingNextSet(this.resultsPage) + "</li>");
                 }
 
                 this.postprocessResults(data, initial);
