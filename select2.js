5859dc8149446e74796699f4b7befd3f71713fe7
Igor Vaynberg <igor.vaynberg@gmail.com>
2012-07-14 00:34:33 +0300

    possible fix for #205

--- select2.js
+++ select2.js
@@ -1598,7 +1598,7 @@
             if (placeholder !== undefined  && this.getVal().length === 0 && this.search.hasClass("select2-focused") === false) {
                 this.search.val(placeholder).addClass("select2-default");
                 // stretch the search box to full width of the container so as much of the placeholder is visible as possible
-                this.search.width("auto");
+                this.resizeSearch();
             } else {
                 // we set this to " " instead of "" and later clear it on focus() because there is a firefox bug
                 // that does not properly render the caret when the field starts out blank
