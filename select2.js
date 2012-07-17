0f3ac0ca2f7a423450714bd19208cc0ce6e32670
Dean Sofer <proloser@hotmail.com>
2012-07-17 16:25:34 -0700

    Fixes ivaynberg/select2#230 (albeit poorly)
    You may choose to implement this a bit more elegantly yourself.

--- select2.js
+++ select2.js
@@ -1935,6 +1935,7 @@
         var args = Array.prototype.slice.call(arguments, 0),
             opts,
             select2,
+            returnSelf = true,
             value, multiple, allowedMethods = ["val", "destroy", "open", "close", "focus", "isFocused", "container", "onSortStart", "onSortEnd", "enable", "disable", "positionDropdown", "data"];
 
         this.each(function () {
@@ -1963,6 +1964,9 @@
                 if (args[0] === "container") {
                     value=select2.container;
                 } else {
+                if (args.length === 1 && (args[0] === 'val' || args[0] === 'data')) {
+                    returnSelf = false;
+                }
                     value = select2[args[0]].apply(select2, args.slice(1));
                 }
                 if (value !== undefined) {return false;}
@@ -1970,7 +1974,7 @@
                 throw "Invalid arguments to select2 plugin: " + args;
             }
         });
-        return (value === undefined) ? this : value;
+        return (value === undefined && returnSelf) ? this : value;
     };
 
     // plugin defaults, accessible to users
