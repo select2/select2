11d04461ca89e7f6ecefc00a1439f76145c452c4
Maurus Cuelenaere <mcuelenaere@gmail.com>
2012-08-08 19:23:56 +0200

    Use Array.reduce instead of jQuery.each (thanks to @ryfeng)

--- select2.js
+++ select2.js
@@ -405,15 +405,9 @@
     }
 
     function countResults(results) {
-        var count = 0;
-        $.each(results, function(i, item) {
-            if (item.children) {
-                count += countResults(item.children);
-            } else {
-                count++;
-            }
-        });
-        return count;
+        return results.reduce(function(count, item) {
+            return count + (item.children ? countResults(item.children) : 1);
+        }, 0);
     }
 
     /**
