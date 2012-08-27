b5f1bedb3538b9c5116f8ccecebd7b0c427f8f37
Sławek Ehlert <slafs@op.pl>
2012-08-27 10:05:43 +0200

    forgot to remove log

--- select2.js
+++ select2.js
@@ -2059,7 +2059,6 @@
                 formatted;
 
             formatted=this.opts.formatSelection(data, choice);
-                    console.log(" 7 " + this.escapeMarkup);
             choice.find("div").replaceWith("<div>"+this.escapeMarkup(formatted)+"</div>");
             choice.find(".select2-search-choice-close")
                 .bind("mousedown", killEvent)
