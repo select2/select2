46d72e653877dbf8267d5dbfad7fe3834cd13934
Troy Zarger <github@webtechedgeinc.com>
2012-09-03 23:59:47 -0400

    Revert "Fix for ValidationEngine &hiding errors on change."
    This reverts commit 20da983ec7380fdeb30a22c8aeca28b705fe8ff2.

--- select2.js
+++ select2.js
@@ -568,7 +568,7 @@
             this.body = thunk(function() { return opts.element.closest("body"); });
 
             if (opts.element.attr("class") !== undefined) {
-                this.container.addClass(opts.element.attr("class").replace(/validate\[[\S ]+] ?/, ''));
+                this.container.addClass(opts.element.attr("class"));
             }
 
             this.container.css(evaluate(opts.containerCss));
@@ -833,11 +833,6 @@
             // some validation frameworks ignore the change event and listen instead to keyup, click for selects
             // so here we trigger the click event manually
             this.opts.element.click();
-
-            // ValidationEngine uses the blur event to hide errors immediately
-            // so here we trigger the blur event manually if so desired
-            if (this.opts.blurOnChange)
-              this.opts.element.blur();
         },
 
 
@@ -2371,8 +2366,7 @@
                 return markup.replace(/&/g, "&amp;");
             }
             return markup;
-        },
-        blurOnChange: false
+        }
     };
 
     // exports
