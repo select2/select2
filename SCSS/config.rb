0529a7a3705c218e665a6d77ca44d285cfc2be98
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2012-10-31 17:52:42 +0100

    Rewritten CSS in SCSS with Compass
    - The CSS file is now generated with SCSS (sass-lang.com)
    - Compass is used for extra features/mixins like vendor prefixing. In
    the future spirte map building culd be included. (compass-style.org)
    - Added a Compass build config.rb

---
+++ SCSS/config.rb
@@ -0,0 +1,18 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = ".."
+sass_dir = "/"
+images_dir = ".."
+javascripts_dir = ".."
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
