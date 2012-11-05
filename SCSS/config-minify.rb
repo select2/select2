ef572a088203a206f5d13782be68941fde7f198d
Allan Kimmer Jensen <allankimmerjensen@gmail.com>
2012-11-05 16:13:58 +0100

    Added both a minify and a expanded version
    Added both a minify and a expanded versionof the compiled css, with two
    config files.

--- SCSS/config-minify.rb
+++ SCSS/config-minify.rb
@@ -9,7 +9,7 @@ javascripts_dir = ".."
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-# output_style = :compressed
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
