81a0e2610a1317580a98c7df39ffde0569765baa
Igor Vaynberg <igor.vaynberg@gmail.com>
2013-02-05 15:02:43 -0800

    improve placeholder management. fixes #712

--- select2.js
+++ select2.js
@@ -2020,7 +2020,7 @@ the specific language governing permissions and limitations under the Apache Lic
             this.search.bind("blur", this.bind(function(e) {
                 this.container.removeClass("select2-container-active");
                 this.search.removeClass("select2-focused");
-                this.clearSearch();
+                if (!this.opened()) this.clearSearch();
                 e.stopImmediatePropagation();
             }));
 
