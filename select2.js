3105665fe6b1bdeef1369ae8680885b8b6547c17
Igor Vaynberg <igor.vaynberg@gmail.com>
2013-02-05 18:34:04 -0800

    release 3.3.0

--- select2.js
+++ select2.js
@@ -1,7 +1,7 @@
 /*
 Copyright 2012 Igor Vaynberg
 
-Version: @@ver@@ Timestamp: @@timestamp@@
+Version: 3.3.0 Timestamp: Tue Feb  5 18:33:54 PST 2013
 
 This software is licensed under the Apache License, Version 2.0 (the "Apache License") or the GNU
 General Public License version 2 (the "GPL License"). You may choose either license to govern your
