2ef47d1c98d50700ff1cb8e32b0bd0edc5ba1830
Lee Baker <lee.rekab@gmail.com>
2013-02-12 14:13:06 +1100

    Adding a method to change the data in the dropdown
    Have added a new method that lets the coder set new data in the
    dropdown.

--- select2.js
+++ select2.js
@@ -1944,6 +1944,14 @@ the specific language governing permissions and limitations under the Apache Lic
             this.focusser.val("");
         },
 
+		setData: function(value) {
+			var val = this.opts.element.val();
+			if (val && val != "") {
+				this.clear();
+			}
+			this.opts.query = local(value);
+		},
+		
         // single
         data: function(value) {
             var data;
@@ -1955,9 +1963,9 @@ the specific language governing permissions and limitations under the Apache Lic
             } else {
                 if (!value || value === "") {
                     this.clear();
-                } else {
-                    this.opts.element.val(!value ? "" : this.id(value));
-                    this.updateSelection(value);
+                } else {							
+					this.opts.element.val(!value ? "" : this.id(value));
+					this.updateSelection(value);				
                 }
             }
         }
@@ -2545,6 +2553,13 @@ the specific language governing permissions and limitations under the Apache Lic
             this.triggerChange();
         },
 
+		// multi
+		setData: function(value) {
+			this.clearSearch();
+			this.opts.query = local(value);
+		},
+
+		
         // multi
         data: function(values) {
             var self=this, ids;
@@ -2568,7 +2583,7 @@ the specific language governing permissions and limitations under the Apache Lic
         var args = Array.prototype.slice.call(arguments, 0),
             opts,
             select2,
-            value, multiple, allowedMethods = ["val", "destroy", "opened", "open", "close", "focus", "isFocused", "container", "onSortStart", "onSortEnd", "enable", "disable", "positionDropdown", "data"];
+            value, multiple, allowedMethods = ["val", "destroy", "opened", "open", "close", "focus", "isFocused", "container", "onSortStart", "onSortEnd", "enable", "disable", "positionDropdown", "data", "setData"];
 
         this.each(function () {
             if (args.length === 0 || typeof(args[0]) === "object") {
