27f736cb56edaba397cc4790ac46d2f8359df303
Kevin Brown <bass5098@gmail.com>
2013-03-04 20:35:55 -0500

    Fixed artifacts in pt-PT translation

--- select2_locale_pt-PT.js
+++ select2_locale_pt-PT.js
@@ -8,7 +8,7 @@
         formatNoMatches: function () { return "Nenhum resultado encontrado"; },
         formatInputTooShort: function (input, min) { var n = min - input.length; return "Introduza " + n + " caracter" + (n == 1 ? "" : "es"); },
         formatInputTooLong: function (input, max) { var n = input.length - max; return "Apague " + n + " caracter" + (n == 1 ? "" : "es"); },
-        formatSelectionTooBig: function (limit) { return "Só é possível selecionar " + limit + " elemento" + (limit == 1 ? "" : "s"); },
+        formatSelectionTooBig: function (limit) { return "SÃ³ Ã© possÃ­vel selecionar " + limit + " elemento" + (limit == 1 ? "" : "s"); },
         formatLoadMore: function (pageNumber) { return "A carregar mais resultados..."; },
         formatSearching: function () { return "A pesquisar..."; }
     });
