ec83e76f5e7fe25a46302f38269830f1598ed8ee
Igor Vaynberg <igor.vaynberg@gmail.com>
2013-04-08 09:57:05 -0700

    Repack Hebrew Translation. closes #1154

--- select2_locale_he.js
+++ select2_locale_he.js
@@ -1,16 +1,16 @@
 /**
- * Select2 Hebrew translation.
- *
- * Author: Yakir Sitbon <http://www.yakirs.net/>
- */
+* Select2 Hebrew translation.
+*
+* Author: Yakir Sitbon <http://www.yakirs.net/>
+*/
 (function ($) {
     "use strict";
 
     $.extend($.fn.select2.defaults, {
-        formatNoMatches: function () { return "לא נמצאו תוצאות"; },
-        formatInputTooShort: function (input, min) { var n = min - input.length; return "אנא הזן " + n + " תווים נוספים"; },
-        formatInputTooLong: function (input, max) { var n = input.length - max; return "אנא הזן " + n + " פחות תווים"; },
-        formatSelectionTooBig: function (limit) { return "הנך יכול לבחור " + limit + " פרטים"; },
+        formatNoMatches: function () { return "לא נמצאו התאמות"; },
+        formatInputTooShort: function (input, min) { var n = min - input.length; return "נא להזין עוד " + n + " תווים נוספים"; },
+        formatInputTooLong: function (input, max) { var n = input.length - max; return "נא להזין פחות " + n + " תווים"; },
+        formatSelectionTooBig: function (limit) { return "ניתן לבחור " + limit + " פריטים"; },
         formatLoadMore: function (pageNumber) { return "טוען תוצאות נוספות..."; },
         formatSearching: function () { return "מחפש..."; }
     });
