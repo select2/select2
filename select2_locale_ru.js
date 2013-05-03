d6b62ca86bea6b52ba490fca867104c6b9ddd2aa
Boccob <lord.boccob@gmail.com>
2013-05-03 18:10:07 +0300

    fixed Russian translation

--- select2_locale_ru.js
+++ select2_locale_ru.js
@@ -8,7 +8,7 @@
         formatNoMatches: function () { return "Совпадений не найдено"; },
         formatInputTooShort: function (input, min) { var n = min - input.length; return "Пожалуйста, введите еще " + n + " символ" + (n == 1 ? "" : ((n > 1)&&(n < 5) ? "а" : "ов")); },
         formatInputTooLong: function (input, max) { var n = input.length - max; return "Пожалуйста, введите на " + n + " символ" + (n == 1 ? "" : ((n > 1)&&(n < 5)? "а" : "ов")) + " меньше"; },
-        formatSelectionTooBig: function (limit) { return "Вы можете выбрать не более " + limit + " элемент" + (limit == 1 ? "" : ((limit > 1)&&(limit < 5)? "а" : "ов")); },
+        formatSelectionTooBig: function (limit) { return "Вы можете выбрать не более " + limit + " элемент" + (limit == 1 ? "а" : "ов"); },
         formatLoadMore: function (pageNumber) { return "Загрузка данных..."; },
         formatSearching: function () { return "Поиск..."; }
     });
