a2f138cddfc6f1b1ba0ac6a38e0ab6c528aa60ec
vikev <lubomirvikev@gmail.com>
2013-07-03 17:29:36 +0300

    Update select2_locale_bg.js

--- select2_locale_bg.js
+++ select2_locale_bg.js
@@ -10,8 +10,8 @@
         formatNoMatches: function () { return "Няма намерени съвпадения"; },
         formatInputTooShort: function (input, min) { var n = min - input.length; return "Моля въведете още " + n + " символ" + (n == 1 ? "" : "а"); },
         formatInputTooLong: function (input, max) { var n = input.length - max; return "Моля въведете с " + n + " по-малко символ" + (n == 1? "" : "а"); },
-        formatSelectionTooBig: function (limit) { return "Можете да направите " + limit + (limit == 1 ? "избор" : "избора"); },
-        formatLoadMore: function (pageNumber) { return "Зареждане на още задачи..."; },
+        formatSelectionTooBig: function (limit) { return "Можете да направите до " + limit + (limit == 1 ? " избор" : " избора"); },
+        formatLoadMore: function (pageNumber) { return "Зареждат се още..."; },
         formatSearching: function () { return "Търсене..."; }
     });
 })(jQuery);
