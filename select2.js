cdcdc98b5583f08bdf6416a3f225cc1c509a13dc
Igor Vaynberg <igor.vaynberg@gmail.com>
2013-07-03 22:42:14 -0700

    copy style attr from original element. fixes #1476

--- select2.js
+++ select2.js
@@ -678,6 +678,7 @@ the specific language governing permissions and limitations under the Apache Lic
 
             syncCssClasses(this.container, this.opts.element, this.opts.adaptContainerCssClass);
 
+            this.container.attr("style", opts.element.attr("style"));
             this.container.css(evaluate(opts.containerCss));
             this.container.addClass(evaluate(opts.containerCssClass));
 
