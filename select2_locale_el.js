da0aecbdd174b8eaf7126355be50aca7ad331408
Nikos Kampitakis <nikamp@forthnetgroup.com>
2013-10-03 14:12:30 +0300

    corrections for the greek (el) locale file

--- select2_locale_el.js
+++ select2_locale_el.js
@@ -8,9 +8,9 @@
 
     $.extend($.fn.select2.defaults, {
         formatNoMatches: function () { return "Δεν βρέθηκαν αποτελέσματα"; },
-        formatInputTooShort: function (input, min) { var n = min - input.length; return "Παρακαλούμε εισάγετε " + n + " περισσότερους χαρακτήρες" + (n == 1 ? "" : "s"); },
-        formatInputTooLong: function (input, max) { var n = input.length - max; return "Παρακαλούμε διαγράψτε " + n + " χαρακτήρες" + (n == 1 ? "" : "s"); },
-        formatSelectionTooBig: function (limit) { return "Μπορείτε να επιλέξετε μόνο " + limit + " αντικείμενο" + (limit == 1 ? "" : "s"); },
+        formatInputTooShort: function (input, min) { var n = min - input.length; return "Παρακαλούμε εισάγετε " + n + " περισσότερο" + (n == 1 ? "" : "υς") + " χαρακτήρ" + (n == 1 ? "α" : "ες"); },
+        formatInputTooLong: function (input, max) { var n = input.length - max; return "Παρακαλούμε διαγράψτε " + n + " χαρακτήρ" + (n == 1 ? "α" : "ες"); },
+        formatSelectionTooBig: function (limit) { return "Μπορείτε να επιλέξετε μόνο " + limit + " αντικείμεν" + (limit == 1 ? "ο" : "α"); },
         formatLoadMore: function (pageNumber) { return "Φόρτωση περισσότερων..."; },
         formatSearching: function () { return "Αναζήτηση..."; }
     });
