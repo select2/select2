0981ab612e3b473dcb5b9d6f6e0998c1f6d39ea7
stan <stan@nashdot.com>
2013-10-10 23:11:27 +0200

    Cleanup unused code

--- select2.js
+++ select2.js
@@ -612,7 +612,6 @@ the specific language governing permissions and limitations under the Apache Lic
             if (token.length > 0) {
                 token = opts.createSearchChoice.call(this, token, selection);
                 if (token !== undefined && token !== null && opts.id(token) !== undefined && opts.id(token) !== null) {
-                    dupe = false;
                     for (i = 0, l = selection.length; i < l; i++) {
                         if (equal(opts.id(token), opts.id(selection[i]))) {
                             dupe = true; break;
@@ -1034,7 +1033,7 @@ the specific language governing permissions and limitations under the Apache Lic
             if (this.mutationCallback === undefined) {
                 this.mutationCallback = function (mutations) {
                     mutations.forEach(sync);
-                }
+                };
             }
 
             // safari and chrome
@@ -2192,7 +2191,7 @@ the specific language governing permissions and limitations under the Apache Lic
 
         // single
         postprocessResults: function (data, initial, noHighlightUpdate) {
-            var selected = 0, self = this, showSearchInput = true;
+            var selected = 0, self = this;
 
             // find the selected element in the result list
 
@@ -2356,7 +2355,7 @@ the specific language governing permissions and limitations under the Apache Lic
                     this.clear(triggerChange);
                 } else {
                     data = this.data();
-                    this.opts.element.val(!value ? "" : this.id(value));
+                    this.opts.element.val(this.id(value));
                     this.updateSelection(value);
                     if (triggerChange) {
                         this.triggerChange({added: value, removed:data});
