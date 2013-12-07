cdef7d9ed6fd0555fa1d101d5533e4c6d6821573
Jimmy Yuen Ho Wong <wyuenho@gmail.com>
2013-12-08 03:28:31 +0800

    Update version for component.json too

--- release.sh
+++ release.sh
@@ -28,9 +28,10 @@ git pull
 
 echo "Updating Version Identifiers"
 
-sed -E -e "s/\"version\": \"([0-9\.]+)\",/\"version\": \"$ver\",/g" -i "" bower.json select2.jquery.json
+sed -E -e "s/\"version\": \"([0-9\.]+)\",/\"version\": \"$ver\",/g" -i "" bower.json select2.jquery.json component.json
 git add bower.json
 git add select2.jquery.json
+git add component.json
 git commit -m "modified version identifiers in descriptors for release $ver"
 git push
  
