dc9259370455b2db382c26be4ed17fa5e8411f52
Alexander Polyanskikh <alexander.polyanskikh@claromentis.com>
2014-01-12 23:49:07 +0000

    Started work on ajax support in select tag

--- select2.js
+++ select2.js
@@ -851,7 +851,7 @@ the specific language governing permissions and limitations under the Apache Lic
 
             if (select) {
                 // these options are not allowed when attached to a select because they are picked up off the element itself
-                $.each(["id", "multiple", "ajax", "query", "createSearchChoice", "initSelection", "data", "tags"], function () {
+                $.each(["id", "multiple", "createSearchChoice", "initSelection", "data", "tags"], function () {
                     if (this in opts) {
                         throw new Error("Option '" + this + "' is not allowed for Select2 when attached to a <select> element.");
                     }
@@ -927,74 +927,73 @@ the specific language governing permissions and limitations under the Apache Lic
                 opts.tags=opts.element.data("select2Tags");
             }
 
-            if (select) {
-                opts.query = this.bind(function (query) {
-                    var data = { results: [], more: false },
-                        term = query.term,
-                        children, placeholderOption, process;
+            if (!("query" in opts)) {
 
-                    process=function(element, collection) {
-                        var group;
-                        if (element.is("option")) {
-                            if (query.matcher(term, element.text(), element)) {
-                                collection.push(self.optionToData(element));
-                            }
-                        } else if (element.is("optgroup")) {
-                            group=self.optionToData(element);
-                            element.children().each2(function(i, elm) { process(elm, group.children); });
-                            if (group.children.length>0) {
-                                collection.push(group);
-                            }
-                        }
-                    };
-
-                    children=element.children();
-
-                    // ignore the placeholder option if there is one
-                    if (this.getPlaceholder() !== undefined && children.length > 0) {
-                        placeholderOption = this.getPlaceholderOption();
-                        if (placeholderOption) {
-                            children=children.not(placeholderOption);
-                        }
+                if ("ajax" in opts) {
+                    ajaxUrl = opts.element.data("ajax-url");
+                    if (ajaxUrl && ajaxUrl.length > 0) {
+                        opts.ajax.url = ajaxUrl;
                     }
-
-                    children.each2(function(i, elm) { process(elm, data.results); });
-
-                    query.callback(data);
-                });
-                // this is needed because inside val() we construct choices from options and there id is hardcoded
-                opts.id=function(e) { return e.id; };
-            } else {
-                if (!("query" in opts)) {
-
-                    if ("ajax" in opts) {
-                        ajaxUrl = opts.element.data("ajax-url");
-                        if (ajaxUrl && ajaxUrl.length > 0) {
-                            opts.ajax.url = ajaxUrl;
-                        }
-                        opts.query = ajax.call(opts.element, opts.ajax);
-                    } else if ("data" in opts) {
-                        opts.query = local(opts.data);
-                    } else if ("tags" in opts) {
-                        opts.query = tags(opts.tags);
-                        if (opts.createSearchChoice === undefined) {
-                            opts.createSearchChoice = function (term) { return {id: $.trim(term), text: $.trim(term)}; };
-                        }
-                        if (opts.initSelection === undefined) {
-                            opts.initSelection = function (element, callback) {
-                                var data = [];
-                                $(splitVal(element.val(), opts.separator)).each(function () {
-                                    var obj = { id: this, text: this },
-                                        tags = opts.tags;
-                                    if ($.isFunction(tags)) tags=tags();
-                                    $(tags).each(function() { if (equal(this.id, obj.id)) { obj = this; return false; } });
-                                    data.push(obj);
-                                });
-
-                                callback(data);
-                            };
-                        }
+                    opts.query = ajax.call(opts.element, opts.ajax);
+                } else if ("data" in opts) {
+                    opts.query = local(opts.data);
+                } else if ("tags" in opts) {
+                    opts.query = tags(opts.tags);
+                    if (opts.createSearchChoice === undefined) {
+                        opts.createSearchChoice = function (term) { return {id: $.trim(term), text: $.trim(term)}; };
                     }
+                    if (opts.initSelection === undefined) {
+                        opts.initSelection = function (element, callback) {
+                            var data = [];
+                            $(splitVal(element.val(), opts.separator)).each(function () {
+                                var obj = { id: this, text: this },
+                                    tags = opts.tags;
+                                if ($.isFunction(tags)) tags=tags();
+                                $(tags).each(function() { if (equal(this.id, obj.id)) { obj = this; return false; } });
+                                data.push(obj);
+                            });
+
+                            callback(data);
+                        };
+                    }
+                } else if (select)
+                {
+                    opts.query = this.bind(function (query) {
+                        var data = { results: [], more: false },
+                            term = query.term,
+                            children, placeholderOption, process;
+
+                        process=function(element, collection) {
+                            var group;
+                            if (element.is("option")) {
+                                if (query.matcher(term, element.text(), element)) {
+                                    collection.push(self.optionToData(element));
+                                }
+                            } else if (element.is("optgroup")) {
+                                group=self.optionToData(element);
+                                element.children().each2(function(i, elm) { process(elm, group.children); });
+                                if (group.children.length>0) {
+                                    collection.push(group);
+                                }
+                            }
+                        };
+
+                        children=element.children();
+
+                        // ignore the placeholder option if there is one
+                        if (this.getPlaceholder() !== undefined && children.length > 0) {
+                            placeholderOption = this.getPlaceholderOption();
+                            if (placeholderOption) {
+                                children=children.not(placeholderOption);
+                            }
+                        }
+
+                        children.each2(function(i, elm) { process(elm, data.results); });
+
+                        query.callback(data);
+                    });
+                    // this is needed because inside val() we construct choices from options and there id is hardcoded
+                    opts.id=function(e) { return e.id; };
                 }
             }
             if (typeof(opts.query) !== "function") {
