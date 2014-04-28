796661a11ddf6bef4d9ebbb776a07ea4065f000f
Dawn Hammond <dawnh@spiceworks.com>
2014-04-28 16:49:08 -0500

    change condition to use length

--- select2.js
+++ select2.js
@@ -2637,7 +2637,7 @@ the specific language governing permissions and limitations under the Apache Lic
                 .attr('for', this.search.attr('id'));
 
             this.search.on("input paste", this.bind(function() {
-                if (this.search.attr('placeholder') && !this.search.val()) return;
+                if (this.search.attr('placeholder') && this.search.val().length == 0) return;
                 if (!this.isInterfaceEnabled()) return;
                 if (!this.opened()) {
                     this.open();
