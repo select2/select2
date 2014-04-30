ac5623291e7b88e377b43a777013339563585c38
Kevin Brown <kbrown@rediker.com>
2014-04-30 19:28:11 -0400

    release 3.4.7

--- select2.js
+++ select2.js
@@ -1,7 +1,7 @@
 /*
 Copyright 2012 Igor Vaynberg
 
-Version: @@ver@@ Timestamp: @@timestamp@@
+Version: 3.4.7 Timestamp: Wed Apr 30 19:28:03 EDT 2014
 
 This software is licensed under the Apache License, Version 2.0 (the "Apache License") or the GNU
 General Public License version 2 (the "GPL License"). You may choose either license to govern your
