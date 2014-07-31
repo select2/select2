e8d93b53155b6e6a7be0f6da09a59e9c9c646a81
Fabrice Daugan <fabrice.daugan@gmail.com>
2014-07-31 08:51:05 +0200

    Update select2.js

--- select2.js
+++ select2.js
@@ -3473,7 +3473,7 @@ the specific language governing permissions and limitations under the Apache Lic
          formatInputTooLong: function (input, max) { var n = input.length - max; return "Please delete " + n + " character" + (n == 1 ? "" : "s"); },
          formatSelectionTooBig: function (limit) { return "You can only select " + limit + " item" + (limit == 1 ? "" : "s"); },
          formatLoadMore: function (pageNumber) { return "Loading more results…"; },
-         formatSearching: function () { return "Searching…"; },
+         formatSearching: function () { return "Searching…"; }
     };
 
     $.extend($.fn.select2.defaults, $.fn.select2.locales['en']);
