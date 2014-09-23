68070d0cac12104122c0c9110d437ebf35a1d003
junhanamaki <jun.hanamaki@gmail.com>
2014-09-23 20:48:18 +0100

    allow markups of subsequent formatLoadMore message
    - setting message with html instead of text
    - invoking escapeMarkup method before setting message

--- select2.js
+++ select2.js
@@ -1655,7 +1655,7 @@ the specific language governing permissions and limitations under the Apache Lic
                     self.postprocessResults(data, false, false);
 
                     if (data.more===true) {
-                        more.detach().appendTo(results).text(evaluate(self.opts.formatLoadMore, self.opts.element, page+1));
+                        more.detach().appendTo(results).html(self.opts.escapeMarkup(evaluate(self.opts.formatLoadMore, self.opts.element, page+1)));
                         window.setTimeout(function() { self.loadMoreIfNeeded(); }, 10);
                     } else {
                         more.remove();
