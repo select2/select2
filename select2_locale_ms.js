de5fe02f5ece3e1a7da4eee61655e2388077e492
od3n <mr.od3n@gmail.com>
2014-10-11 10:32:31 +0800

    updated Malay translation

--- select2_locale_ms.js
+++ select2_locale_ms.js
@@ -2,11 +2,13 @@
  * Select2 Malay translation.
  * 
  * Author: Kepoweran <kepoweran@gmail.com>
+ * Author: Salahuddin Hairai <mr.od3n@gmail.com>
  */
 (function ($) {
     "use strict";
 
     $.fn.select2.locales['ms'] = {
+        formatMatches: function (matches) { if (matches === 1) { return "Satu keputusan ditemui, tekan enter untuk memilih."; } return matches + " keputusan ditemui, gunakan kekunci anak panah ke atas dan ke bawah untuk menavigasi."; },
         formatNoMatches: function () { return "Tiada padanan yang ditemui"; },
         formatInputTooShort: function (input, min) { var n = min - input.length; return "Sila masukkan " + n + " aksara lagi"; },
         formatInputTooLong: function (input, max) { var n = input.length - max; return "Sila hapuskan " + n + " aksara"; },
