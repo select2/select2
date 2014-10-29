d1c19eb012a6688ff54c48283d3a5463429efe4e
Bjørn Johansen <post@bjornjohansen.no>
2014-10-29 22:36:54 +0100

    Added the formatMatches string

--- select2_locale_nb.js
+++ select2_locale_nb.js
@@ -8,6 +8,7 @@
     "use strict";
 
     $.fn.select2.locales['nb'] = {
+        formatMatches: function (matches) { if (matches === 1) { return "Ett resultat er tilgjengelig, trykk enter for å velge det."; } return matches + " resultater er tilgjengelig. Bruk piltastene opp og ned for å navigere."; },
         formatNoMatches: function () { return "Ingen treff"; },
         formatInputTooShort: function (input, min) { var n = min - input.length; return "Vennligst skriv inn " + n + (n>1 ? " flere tegn" : " tegn til"); },
         formatInputTooLong: function (input, max) { var n = input.length - max; return "Vennligst fjern " + n + " tegn"; },
