f1e86470ca08953d802489e6c0e0928674dc08cb
Kevin Brown <kevin@kevinbrown.in>
2015-01-02 18:15:59 -0500

    Clone the options for group elements
    This will always clone the options that are passed in when
    initializing Select2 on multiple objects (like `$("select")`) so
    it doesn't always assume that the configuration is the exact same.
    This prevents any reference-based tricks for modifying the data
    within Select2, but some would look at that as an upside.

--- dist/js/select2.js
+++ dist/js/select2.js
@@ -4431,13 +4431,17 @@ define('jquery.select2',[
 
       if (typeof options === 'object') {
         this.each(function () {
-          var instance = new Select2($(this), options);
+          var instanceOptions = $.extend({}, options, true);
+
+          var instance = new Select2($(this), instanceOptions);
         });
+
+        return this;
       } else if (typeof options === 'string') {
         var instance = this.data('select2');
         var args = Array.prototype.slice.call(arguments, 1);
 
-        instance[options](args);
+        return instance[options](args);
       } else {
         throw new Error('Invalid arguments for Select2: ' + options);
       }
