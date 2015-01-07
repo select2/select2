8a2cdac8ceebf8b00c2c6e370a80731846b031a1
ktaras <ktarasx@gmail.com>
2015-01-08 00:14:57 +0200

    Adds Ukrainian localization

---
+++ src/js/select2/i18n/uk.js
@@ -0,0 +1,40 @@
+define(function () {
+  function ending(count, one, couple, more) {
+    if ([11,12,13,14].indexOf(count % 100) !== -1) {
+      return more;
+    }
+    if (count % 10 === 1) {
+      return one;
+    }
+    if ([2,3,4].indexOf(count % 10) !== -1) {
+      return couple;
+    }
+    return more;
+  }
+
+  return {
+    errorLoading: function () {
+      return 'Неможливо завантажити результати';
+    },
+    inputTooLong: function (args) {
+      var overChars = args.input.length - args.maximum;
+      return 'Будь ласка, видаліть ' + overChars + ' ' + ending(args.maximum, 'літеру', 'літери', 'літер');
+    },
+    inputTooShort: function (args) {
+      var remainingChars = args.minimum - args.input.length;
+      return 'Будь ласка, введіть ' + remainingChars + ' або більше літер';
+    },
+    loadingMore: function () {
+      return 'Завантаження інших результатів…';
+    },
+    maximumSelected: function (args) {
+      return 'Ви можете вибрати лише ' + args.maximum + ' ' + ending(args.maximum, 'пункт', 'пункти', 'пунктів');
+    },
+    noResults: function () {
+      return 'Нічого не знайдено';
+    },
+    searching: function () {
+      return 'Пошук…';
+    }
+  };
+});
