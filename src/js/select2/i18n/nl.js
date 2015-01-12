c318cde916868dca5bc1d80c51235084200c716f
Tim <t.schuitemaker@gen25.com>
2015-01-12 15:39:27 +0100

    Added dutch translations
    nl.js
    
    Signed-off-by: Kevin Brown <kevin@kevinbrown.in>

---
+++ src/js/select2/i18n/nl.js
@@ -0,0 +1,42 @@
+define(function () {
+  // Dutch
+  return {
+    errorLoading: function () {
+      return 'De resultaten konden niet worden geladen.';
+    },
+    inputTooLong: function (args) {
+      var overChars = args.input.length - args.maximum;
+
+      var message = 'Gelieve ' + overChars + ' karakters te verwijderen';
+
+      return message;
+    },
+    inputTooShort: function (args) {
+      var remainingChars = args.minimum - args.input.length;
+
+      var message = 'Gelieve ' + remainingChars +
+        ' of meer karakters in te voeren';
+
+      return message;
+    },
+    loadingMore: function () {
+      return 'Meer resultaten laden…';
+    },
+    maximumSelected: function (args) {
+      var message = 'Er kunnen maar ' + args.maximum + ' item';
+
+      if (args.maximum != 1) {
+        message += 's';
+      }
+      message += ' worden geselecteerd';
+
+      return message;
+    },
+    noResults: function () {
+      return 'Geen resultaten gevonden…';
+    },
+    searching: function () {
+      return 'Zoeken…';
+    }
+  };
+});
