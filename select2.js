04ac5cd843bbaeb7a6d899d20627b231c7c94864
Boro Sitnikovski <bsitnikovski@sugarcrm.com>
2015-01-27 12:23:27 +0100

    Re-initialize container in a case where destroy() then initSelection() is called. Similar to https://github.com/select2/select2/issues/2587, caused by https://github.com/select2/select2/commit/6156abc2a882baf14d7b0f6e7ab01c0ce10c103d.

--- select2.js
+++ select2.js
@@ -2335,6 +2335,9 @@ the specific language governing permissions and limitations under the Apache Lic
                 this.setPlaceholder();
             } else {
                 var self = this;
+                if (this.container === null) {
+                    this.init(this.opts);
+                }
                 this.opts.initSelection.call(null, this.opts.element, function(selected){
                     if (selected !== undefined && selected !== null) {
                         self.updateSelection(selected);
