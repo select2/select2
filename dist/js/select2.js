1349579192e36ecce43ca1714aec07073fdd825d
Kevin Brown <kevin@kevinbrown.in>
2015-02-06 19:54:36 -0500

    Respect `-1` with `minimumResultsForSearch`
    In past versions of Select2, supplying a value less than zero to
    `minimumResultsForSearch` acted the same as providing a high value,
    such as `Infinity`.  This corrects the regression by mapping anything
    less than zero to the value `Infinity`.
    
    This closes https://github.com/select2/select2/issues/2994.

--- dist/js/select2.js
+++ dist/js/select2.js
@@ -3993,6 +3993,10 @@ define('select2/dropdown/minimumResultsForSearch',[
   function MinimumResultsForSearch (decorated, $element, options, dataAdapter) {
     this.minimumResultsForSearch = options.get('minimumResultsForSearch');
 
+    if (this.minimumResultsForSearch < 0) {
+      this.minimumResultsForSearch = Infinity;
+    }
+
     decorated.call(this, $element, options, dataAdapter);
   }
 
--- src/js/select2/dropdown/minimumResultsForSearch.js
+++ src/js/select2/dropdown/minimumResultsForSearch.js
@@ -20,6 +20,10 @@ define([
   function MinimumResultsForSearch (decorated, $element, options, dataAdapter) {
     this.minimumResultsForSearch = options.get('minimumResultsForSearch');
 
+    if (this.minimumResultsForSearch < 0) {
+      this.minimumResultsForSearch = Infinity;
+    }
+
     decorated.call(this, $element, options, dataAdapter);
   }
 
