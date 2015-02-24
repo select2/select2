5ec852e18ce3d7828a5ccdbd897289f4b0e566cc
Kevin Brown <kevin@kevinbrown.in>
2015-02-24 12:23:09 -0500

    Added documentation for `<input type="text" />`
    This adds basic documentation explaining that there are compatibility
    decorators for `<input type="text" />` support. This is not fully
    compatible with all features and requires the use of an external
    adapter like the `ArrayAdapter` or `AjaxAdapter` to provide a
    reasonable way to query results.
    
    This also triggers a warning if the adapter is used with a hidden
    input, as the degraded functionality when there is no JavaScript
    support should be discouraged.

--- src/js/select2/compat/inputData.js
+++ src/js/select2/compat/inputData.js
@@ -5,6 +5,16 @@ define([
     this._currentData = [];
     this._valueSeparator = options.get('valueSeparator') || ',';
 
+    if ($element.prop('type') === 'hidden') {
+      if (console && console.warn) {
+        console.warn(
+          'Select2: Using a hidden input with Select2 is no longer ' +
+          'supported and may stop working in the future. It is recommended ' +
+          'to use a `<select>` element instead.'
+        );
+      }
+    }
+
     decorated.call(this, $element, options);
   }
 
