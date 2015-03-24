e10f3b7dae44915af6adaf28695ecd30752d14da
Brankub <kubrakov.i@gmail.com>
2015-03-24 14:44:23 +0200

    fixes #3169

--- src/js/select2/dropdown/selectOnClose.js
+++ src/js/select2/dropdown/selectOnClose.js
@@ -8,19 +8,24 @@ define([
 
     decorated.call(this, container, $container);
 
-    container.on('close', function () {
-      self._handleSelectOnClose();
+    container.on('close', function (evt) {
+      self._handleSelectOnClose(evt);
     });
   };
 
-  SelectOnClose.prototype._handleSelectOnClose = function () {
+  SelectOnClose.prototype._handleSelectOnClose = function (evt) {
+    var self = this;
+
     var $highlightedResults = this.getHighlightedResults();
 
     if ($highlightedResults.length < 1) {
       return;
     }
 
-    $highlightedResults.trigger('mouseup');
+    self.trigger('select', {
+        originalEvent: evt,
+        data: $highlightedResults.data('data')
+    });
   };
 
   return SelectOnClose;
