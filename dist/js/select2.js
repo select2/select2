5b2cdc035e7e124ff77aef692d5d3bddab0fb453
Seán de Búrca <leftmostcat@gmail.com>
2015-04-26 16:41:41 -0600

    Update compiled files for crash fix

--- dist/js/select2.js
+++ dist/js/select2.js
@@ -5327,6 +5327,11 @@ S2.define('jquery.select2',[
   // Force jQuery.mousewheel to be loaded if it hasn't already
   require('jquery.mousewheel');
 
+  if (typeof $.fn === 'undefined') {
+    // If we're not client-side, jquery isn't fully loaded; we don't need to be
+    return null;
+  }
+
   if ($.fn.select2 == null) {
     // All methods that should return the element
     var thisMethods = ['open', 'close', 'destroy'];
@@ -5396,7 +5401,9 @@ S2.define('jquery.mousewheel',[
   // Hold the AMD module references on the jQuery function that was just loaded
   // This allows Select2 to use the internal loader outside of this file, such
   // as in the language files.
-  jQuery.fn.select2.amd = S2;
+  if (typeof jQuery.fn !== 'undefined') {
+    jQuery.fn.select2.amd = S2;
+  }
 
   // Return the Select2 instance for anyone who is importing it.
   return select2;
