7974fd166294f1ea8653a8ced5ddb506dd23d532
Meir Cohen <c.t.meir@gmail.com>
2015-11-07 17:54:00 +0200

    Corrected a few Hebrew translations
    About  `errorLoading`:
    Before the correction, the translation was: "התוצאות לא נטענו בהלכה".
    The word "בהלכה" is wrong and should be spelled "כהלכה", but besides that, this translations means "The results were loaded improperly", while my corrected translation means "Error while loading results", which is what needed.
    
    About `inputTooLong`, `inputTooShort`, `maximumSelected`:
    In Hebrew, it's not acceptable to have the digit 1 for representing a single object. You should use the actual word "one", which is "אחד" in Hebrew together with the singular noun (e.g. "one item" / "one character", instead of "1 item" / "1 character").
    **These 3 parts needed a critical fix anyway**, because they were addind the English letter 's' to the end of the Hebrew string.
    
    About `loadingMore`:
    The translation was "טען תוצאות נוספות" which means "Load more results", as an instruction, probably a typo.
    I changed "טען" to "טוען" so now it means "loading more results", which is the correct form.
    
    This closes https://github.com/select2/select2/pull/3911

--- src/js/select2/i18n/he.js
+++ src/js/select2/i18n/he.js
@@ -2,15 +2,17 @@ define(function () {
   // Hebrew
   return {
     errorLoading: function () {
-      return 'התוצאות לא נטענו בהלכה';
+      return 'שגיאה בטעינת התוצאות';
     },
     inputTooLong: function (args) {
       var overChars = args.input.length - args.maximum;
 
-      var message = 'נא למחוק ' + overChars + ' תווים';
+      var message = 'נא למחוק ';
 
-      if (overChars != 1) {
-        message += 's';
+      if (overChars === 1) {
+        message += 'תו אחד';
+      } else {
+        message += overChars + ' תווים';
       }
 
       return message;
@@ -18,18 +20,28 @@ define(function () {
     inputTooShort: function (args) {
       var remainingChars = args.minimum - args.input.length;
 
-      var message = 'נא להכניס ' + remainingChars + ' תווים או יותר';
+      var message = 'נא להכניס ';
+
+      if (remainingChars === 1) {
+        message += 'תו אחד';
+      } else {
+        message += remainingChars + ' תווים';
+      }
+
+      message += ' או יותר';
 
       return message;
     },
     loadingMore: function () {
-      return 'טען תוצאות נוספות…';
+      return 'טוען תוצאות נוספות…';
     },
     maximumSelected: function (args) {
-      var message = 'באפשרותך לבחור רק ' + args.maximum + ' פריטים';
+      var message = 'באפשרותך לבחור עד ';
 
-      if (args.maximum != 1) {
-        message += 's';
+      if (args.maximum === 1) {
+        message += 'פריט אחד';
+      } else {
+        message += args.maximum + ' פריטים';
       }
 
       return message;
