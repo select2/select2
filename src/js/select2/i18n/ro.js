72d905f9e026d49e7c600f37a1ce742c404654d7
Radu Ungureanu <radu.ungureanu@outlook.com>
2016-02-24 15:20:25 +0200

    Romanian localization
    - updated transaltions
    - added translation for errorLoading
    - issue fixes - compare characters length instead of message when adding terminal character
    
    This closes https://github.com/select2/select2/pull/4208

--- src/js/select2/i18n/ro.js
+++ src/js/select2/i18n/ro.js
@@ -1,13 +1,15 @@
 define(function () {
   // Romanian
   return {
+    errorLoading: function () {
+      return 'Rezultatele nu au putut fi incărcate.';
+    },
     inputTooLong: function (args) {
       var overChars = args.input.length - args.maximum;
 
-      var message = 'Vă rugăm să introduceți mai puțin de ' + overChars;
-      message += ' caracter';
+      var message = 'Vă rugăm să ștergeți' + overChars + ' caracter';
 
-      if (message !== 1) {
+      if (overChars !== 1) {
         message += 'e';
       }
 
@@ -16,30 +18,26 @@ define(function () {
     inputTooShort: function (args) {
       var remainingChars = args.minimum - args.input.length;
 
-      var message = 'Vă rugăm să introduceți incă ' + remainingChars;
-      message += ' caracter';
-
-      if (message !== 1) {
-        message += 'e';
-      }
+      var message = 'Vă rugăm să introduceți ' + remainingChars +
+        'sau mai multe caractere';
 
       return message;
     },
     loadingMore: function () {
-      return 'Se încarcă…';
+      return 'Se încarcă mai multe rezultate…';
     },
     maximumSelected: function (args) {
       var message = 'Aveți voie să selectați cel mult ' + args.maximum;
       message += ' element';
 
-      if (message !== 1) {
+      if (args.maximum !== 1) {
         message += 'e';
       }
 
       return message;
     },
     noResults: function () {
-      return 'Nu a fost găsit nimic';
+      return 'Nu au fost găsite rezultate';
     },
     searching: function () {
       return 'Căutare…';
