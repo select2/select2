fd69e273d0ccb33a32a676efb6a76f0427a917e8
roman <rgnevashev@gmail.com>
2016-03-10 11:56:50 +0300

    update

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'rgnevashev:select2',
   version: '4.0.2',
   summary: 'Select2 is a jQuery based replacement for select boxes',
-  git: 'https://github.com/rgnevashev/bootstrap-datepicker',
+  git: 'https://github.com/rgnevashev/select2',
   documentation: 'README.md'
 });
 
@@ -10,6 +10,7 @@ Package.onUse(function(api) {
   api.use(['jquery@1.11.4']);
   api.addFiles([
     "dist/css/select2.css",
+    "dist/css/select2-bootstrap.css",
     "dist/js/select2.js"
-  ], 'client');
+  ], 'client', {bare: true});
 });
