5fd270c9e4e1698ddecab7a44a80273b5901290c
Nadim Afana <nafana@qti.qualcomm.com>
2016-05-08 19:32:41 -0700

    Added an integration test to ensure issue does not come back in the future.

--- tests/integration/jquery-calls.js
+++ tests/integration/jquery-calls.js
@@ -26,3 +26,31 @@ test('multiple elements with arguments works', function (assert) {
     'The call should also change the value on the second element'
   );
 });
+
+test('initializes correctly when jQuery $.data contains cyclic reference object', function (assert) {
+  var $ = require('jquery');
+  require('jquery.select2');
+
+  var $select = $(
+    '<select>' +
+      '<option>One</option>' +
+      '<option>Two</option>' +
+      '<option value="3" selected>Three</option>' +
+    '</select>'
+  );
+
+  // Add a circular reference object using jQuery.
+  var recursiveObject = {};
+
+  recursiveObject['same'] =  recursiveObject;
+
+  $select.data('same', recursiveObject);
+
+  $select.select2();
+
+  assert.equal(
+    $select.val(),
+    '3',
+    'The option value should be pulled correctly'
+  );
+});
