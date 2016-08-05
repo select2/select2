44d6efe2383a979145d2de5efd690f490fca771f
გიორგი <giokaxo@gmail.com>
2016-08-05 21:18:04 +0400

    update Georgian translation

--- src/js/select2/i18n/ka.js
+++ src/js/select2/i18n/ka.js
@@ -1,37 +1,44 @@
-/*! Select2 4.0.3 | https://github.com/select2/select2/blob/master/LICENSE.md */
-(function() {
-    if (jQuery && jQuery.fn && jQuery.fn.select2 && jQuery.fn.select2.amd) var e = jQuery.fn.select2.amd;
-    return e.define("select2/i18n/ka", [], function() {
-        return {
-            errorLoading: function() {
-                return "სია ცარიელია"
-            },
-            inputTooLong: function(e) {
-                var t = e.input.length - e.maximum,
-                    n = "გთხოვთ წაშალოთ " + t + " სიმბოლო";
-                return t != 1 && (n += "s"), n
-            },
-            inputTooShort: function(e) {
-                var t = e.minimum - e.input.length,
-                    n = "ჷთხოვთ აკრიფოთ " + t + " ან მეტი სიმბოლო";
-                return n
-            },
-            loadingMore: function() {
-                return "შედეგები იტვირთება…"
-            },
-            maximumSelected: function(e) {
-                var t = "თქვენ შეგიძლიათ მხოლოდ " + e.maximum + " ელემენტის არჩევა";
-                return e.maximum != 1 && (t += "s"), t
-            },
-            noResults: function() {
-                return "ვერაფერი მოიძებნა"
-            },
-            searching: function() {
-                return "ძიება..."
-            }
-        }
-    }), {
-        define: e.define,
-        require: e.require
+define(function () {
+  // Georgian
+  return {
+    errorLoading: function () {
+      return 'სია ცარიელია.';
+    },
+    inputTooLong: function (args) {
+      var overChars = args.input.length - args.maximum;
+
+      var message = 'გთხოვთ წაშალოთ ' + overChars + ' სიმბოლო';
+
+      if (overChars != 1) {
+        message += 's';
+      }
+
+      return message;
+    },
+    inputTooShort: function (args) {
+      var remainingChars = args.minimum - args.input.length;
+
+      var message = 'ჷთხოვთ აკრიფოთ ' + remainingChars + ' ან მეტი სიმბოლო';
+
+      return message;
+    },
+    loadingMore: function () {
+      return 'შედეგები იტვირთება…';
+    },
+    maximumSelected: function (args) {
+      var message = 'თქვენ შეგიძლიათ მხოლოდ ' + args.maximum + '  ელემენტის არჩევა';
+
+      if (args.maximum != 1) {
+        message += 's';
+      }
+
+      return message;
+    },
+    noResults: function () {
+      return 'ვერაფერი მოიძებნა';
+    },
+    searching: function () {
+      return 'ძიება...';
     }
-})();
+  };
+});
