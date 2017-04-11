da5527c6041531e989e59eefe28b6910835e1c24
ToreOlavKristiansen <tore@collabora.no>
2017-04-11 14:16:44 +0200

    Fixed Chrome issue: lost focus after selecting a value

--- src/js/select2/selection/base.js
+++ src/js/select2/selection/base.js
@@ -82,6 +82,9 @@ define([
       self.$selection.removeAttr('aria-owns');
 
       self.$selection.focus();
+      window.setTimeout(function () {
+        self.$selection.focus();
+      }, 0);
 
       self._detachCloseHandler(container);
     });
