3561810a64ae78935209654b98fcd25c225d8900
alexweissman <alex@alexanderweissman.com>
2017-09-24 21:43:57 -0400

    Fix line lengths to keep Travis happy

--- src/js/select2/i18n/fr.js
+++ src/js/select2/i18n/fr.js
@@ -7,18 +7,21 @@ define(function () {
     inputTooLong: function (args) {
       var overChars = args.input.length - args.maximum;
 
-      return 'Supprimez ' + overChars + ' caractère' + (overChars > 1) ? 's' : '';
+      return 'Supprimez ' + overChars + ' caractère' +
+        (overChars > 1) ? 's' : '';
     },
     inputTooShort: function (args) {
       var remainingChars = args.minimum - args.input.length;
 
-      return 'Saisissez au moins ' + remainingChars + ' caractère' + (remainingChars > 1) ? 's' : '';
+      return 'Saisissez au moins ' + remainingChars + ' caractère' +
+        (remainingChars > 1) ? 's' : '';
     },
     loadingMore: function () {
       return 'Chargement de résultats supplémentaires…';
     },
     maximumSelected: function (args) {
-      return 'Vous pouvez seulement sélectionner ' + args.maximum + ' élément' + (args.maximum > 1) ? 's' : '';
+      return 'Vous pouvez seulement sélectionner ' + args.maximum +
+        ' élément' + (args.maximum > 1) ? 's' : '';
     },
     noResults: function () {
       return 'Aucun résultat trouvé';
