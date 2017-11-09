c8679ada00905d921be5ec9169e24782d25586ad
Jason Young <jason.young@procentive.com>
2017-11-09 14:12:10 -0600

    test verifying focus of search element upon opening Select2 with API call.

--- tests/integration/jquery-calls.js
+++ tests/integration/jquery-calls.js
@@ -29,6 +29,41 @@ test('multiple elements with arguments works', function (assert) {
   );
 });
 
+test('open focuses search input', function (assert) {
+  var done = assert.async();
+
+  var $ = require('jquery');
+  require('jquery.select2');
+
+  var $select2 = $(
+    '<select>' +
+      '<option>1</option>' +
+      '<option>2</option>' +
+    '</select>'
+  );
+  $('.qunit-fixture').append($select2);
+
+  $select2.select2({minimumResultsForSearch: 1});
+
+  var activeElement = document.activeElement;
+  assert.equal($select2.has(':focus').length, 0, 'Select2 elements should not be focused. Focused element was ' + activeElement);
+  assert.equal($('.select2').has(':focus').length, 0, 'Original select field should not be focused. Focused element was ' + activeElement);
+
+  $select2.select2('open');
+
+  setTimeout(function() {
+    var $search = $('input.select2-search__field');
+    assert.equal($search.length, 1, 'Search input should exist.');
+    assert.ok($.contains(document.documentElement, $search[0]), 'Search input should be in DOM.');
+    assert.ok($search.is(':visible'), 'Search input should be visible.');
+
+    activeElement = document.activeElement;
+    assert.ok($search.is(':focus'), 'Search input should be focused. Focused element was ' + activeElement);
+
+    done();
+  });
+});
+
 test('initializes when jQuery $.data contains' +
   ' cyclic reference', function (assert) {
   var $ = require('jquery');
@@ -57,3 +92,4 @@ test('initializes when jQuery $.data contains' +
     'The option value should be pulled correctly'
   );
 });
+
