f5d362185b201890beb70fe1c1e9d9c74f1fea49
Mikhail Titov <mlt@gmx.us>
2018-09-24 20:54:34 -0500

    Test for data-dropdown-parent

--- tests/dropdown/positioning-tests.js
+++ tests/dropdown/positioning-tests.js
@@ -58,6 +58,62 @@ test('appends to the dropdown parent', function (assert) {
     );
 });
 
+test('appends to the dropdown parent with data attribute', function (assert) {
+    assert.expect(4);
+
+    var $ = require('jquery');
+
+    var $select = $('<select data-dropdown-parent="#parent"></select>');
+    var $parent = $('<div id="parent"></div>');
+
+    var $container = $('<span></span>');
+    var container = new MockContainer();
+
+    $parent.appendTo($('#qunit-fixture'));
+    $select.appendTo($parent);
+
+    var Utils = require('select2/utils');
+    var Options = require('select2/options');
+
+    var Dropdown = require('select2/dropdown');
+    var AttachBody = require('select2/dropdown/attachBody');
+
+    var DropdownAdapter = Utils.Decorate(Dropdown, AttachBody);
+
+    var dropdown = new DropdownAdapter($select, new Options({}, $select));
+
+    assert.equal(
+        $parent.children().length,
+        1,
+        'Only the select should be in the container'
+    );
+
+    var $dropdown = dropdown.render();
+
+    dropdown.bind(container, $container);
+
+    dropdown.position($dropdown, $container);
+
+    assert.equal(
+        $parent.children().length,
+        1,
+        'The dropdown should not be placed until after it is opened'
+    );
+
+    dropdown._showDropdown();
+
+    assert.equal(
+        $parent.children().length,
+        2,
+        'The dropdown should now be in the container as well'
+    );
+
+    assert.ok(
+        $.contains($parent[0], $dropdown[0]),
+        'The dropdown should be contained within the parent container'
+    );
+});
+
 test('dropdown is positioned down with static margins', function (assert) {
     var $ = require('jquery');
     var $select = $('<select></select>');
