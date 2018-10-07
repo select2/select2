dd251f63644df252fec9f9fa73bb815351e5eb41
thanhpd <thanhpd.mac@gmail.com>
2018-10-07 18:34:09 +0700

    Update Vietnamese (vi) translation

--- src/js/select2/i18n/vi.js
+++ src/js/select2/i18n/vi.js
@@ -4,18 +4,15 @@ define(function () {
     inputTooLong: function (args) {
       var overChars = args.input.length - args.maximum;
 
-      var message = 'Vui lòng nhập ít hơn ' + overChars + ' ký tự';
-
-      if (overChars != 1) {
-        message += 's';
-      }
+      var message = 'Vui lòng xóa bớt ' + overChars + ' ký tự';
 
       return message;
     },
     inputTooShort: function (args) {
       var remainingChars = args.minimum - args.input.length;
 
-      var message = 'Vui lòng nhập nhiều hơn ' + remainingChars + ' ký tự';
+      var message = 'Vui lòng nhập thêm từ ' + remainingChars +
+                    ' ký tự trở lên';
 
       return message;
     },
@@ -33,7 +30,7 @@ define(function () {
     searching: function () {
       return 'Đang tìm…';
     },
-    removeAllItems: function () {     
+    removeAllItems: function () {
       return 'Xóa tất cả các mục';
     }
   };
