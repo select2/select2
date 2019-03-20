98e7969e88ca01219905dd30fe83ab517ea9cfe3
LAPTOP-4ONCPGNL\Mtran <mtran0011@outlook.com>
2019-03-21 09:52:07 +1100

    Fixes for the priority of Select2 language picker mechanism.

--- src/js/select2/options.js
+++ src/js/select2/options.js
@@ -37,7 +37,7 @@ define([
     if (this.options.language == null) {
       if ($e.prop('lang')) {
         this.options.language = $e.prop('lang').toLowerCase();
-      } else if ($e.closest('[lang]').prop('lang')) {
+      } else if (typeof $.fn.select2.defaults.defaults.language !== 'string' && $e.closest('[lang]').prop('lang')) {
         this.options.language = $e.closest('[lang]').prop('lang');
       }
     }
