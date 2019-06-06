66f50e970e1e7c1defc4856851c8eb6ccc97474c
José Luis Salinas <sadi78@gmail.com>
2019-06-06 21:35:06 +0200

    Update tags-tests.js

--- tests/data/tags-tests.js
+++ tests/data/tags-tests.js
@@ -82,7 +82,7 @@ test('does not create option if text is same but lowercase', function (assert) {
 test('creates option if createTagOnMatch is true', function (assert) {
   assert.expect(8);
 
-  var optionsCreateTagOnMatch = new Options({
+  var newOptions = new Options({
     tags: true,
     createTagOnMatch: 1,
     createTag: function (params) {
@@ -96,26 +96,26 @@ test('creates option if createTagOnMatch is true', function (assert) {
         id: '_' + term + '_',
         text: '*' + term + '*',
         newTag: true
-      }
+      };
     }
   });
 
-  var data = new SelectTags($('#qunit-fixture .single'), optionsCreateTagOnMatch);
+  var data = new SelectTags($('#qunit-fixture .single'), newOptions);
 
-  var num_query_answers = 0;
+  var numQueryAnswers = 0;
 
   data.query({
     term: 'One'
   }, function (data) {
-    num_query_answers += 1;
+    numQueryAnswers += 1;
 
-    if ( num_query_answers == 1 ) {
+    if ( numQueryAnswers == 1 ) {
       assert.equal(data.results.length, 1);
       return;
-    } else if ( num_query_answers == 2 ) {
+    } else if ( numQueryAnswers == 2 ) {
       assert.equal(data.results.length, 2);
     } else {
-      assert.equal(true, false, 'The query should never return more than 2 answers.');
+      assert.equal(true, false, "Query should't return more than 2 answers.");
     }
 
     var item0 = data.results[0];
