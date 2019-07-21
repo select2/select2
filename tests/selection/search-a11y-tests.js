3d534d59ab7086cd6688d22f61c557dbffe191c1
Andrea Fercia <a.fercia@gmail.com>
2019-07-21 09:45:47 -0400

    Set aria-activedescendent and aria-owns on selection search
    This is a reduced version of a5ab08b49cb which is split out to only
    set the `aria-activedescendent` and `aria-owns` attributes on the
    search box located within the selection container. This is the search
    box used within a multiple select, and previously it did not always
    set these two attributes correctly.
    
    One major change here is that we clear the `aria-activedescendent`
    attribute if the result that is selected does not have an ID. This
    was not being done previously, instead the attribute was still
    containing the old value, and it meant that sometimes the wrong
    result was being pointed to.
    
    The test coverage for this was also expanded to ensure that these
    attributes are properly being set.

--- tests/selection/search-a11y-tests.js
+++ tests/selection/search-a11y-tests.js
@@ -16,6 +16,9 @@ test('aria-autocomplete attribute is present', function (assert) {
   var selection = new CustomSelection($select, options);
   var $selection = selection.render();
 
+  var container = new MockContainer();
+  selection.bind(container, $('<span></span>'));
+
   // Update the selection so the search is rendered
   selection.update([]);
 
@@ -23,6 +26,81 @@ test('aria-autocomplete attribute is present', function (assert) {
     $selection.find('input').attr('aria-autocomplete'),
     'list',
     'The search box is marked as autocomplete'
+  );
+});
+
+test('aria-activedescendant should not be set initiailly', function (assert) {
+  var $select = $('#qunit-fixture .multiple');
+
+  var CustomSelection = Utils.Decorate(MultipleSelection, InlineSearch);
+  var selection = new CustomSelection($select, options);
+  var $selection = selection.render();
+
+  var container = new MockContainer();
+  selection.bind(container, $('<span></span>'));
+
+  // Update the selection so the search is rendered
+  selection.update([]);
+
+  var $search = $selection.find('input');
+
+  assert.ok(
+    !$search.attr('aria-activedescendant'),
+    'The search box should not point to anything when it is first rendered'
+  );
+});
+
+test('aria-activedescendant should be set after highlight', function (assert) {
+  var $select = $('#qunit-fixture .multiple');
+
+  var CustomSelection = Utils.Decorate(MultipleSelection, InlineSearch);
+  var selection = new CustomSelection($select, options);
+  var $selection = selection.render();
+
+  var container = new MockContainer();
+  selection.bind(container, $('<span></span>'));
+
+  // Update the selection so the search is rendered
+  selection.update([]);
+
+  container.trigger('results:focus', {
+    data: {
+      _resultId: 'test'
+    }
+  });
+
+  var $search = $selection.find('input');
+
+  assert.equal(
+    $search.attr('aria-activedescendant'),
+    'test',
+    'The search is pointing to the focused result'
+  );
+});
+
+test('activedescendant should remove if there is no ID', function (assert) {
+  var $select = $('#qunit-fixture .multiple');
+
+  var CustomSelection = Utils.Decorate(MultipleSelection, InlineSearch);
+  var selection = new CustomSelection($select, options);
+  var $selection = selection.render();
+
+  var container = new MockContainer();
+  selection.bind(container, $('<span></span>'));
+
+  // Update the selection so the search is rendered
+  selection.update([]);
+
+  var $search = $selection.find('input');
+  $search.attr('aria-activedescendant', 'test');
+
+  container.trigger('results:focus', {
+    data: {}
+  });
+
+  assert.ok(
+    !$search.attr('aria-activedescendant'),
+    'There is no result for the search to be pointing to'
   );
 });
 
@@ -47,5 +125,73 @@ test('aria-activedescendant should be removed when closed', function (assert) {
   assert.ok(
     !$search.attr('aria-activedescendant'),
     'There is no active descendant when the dropdown is closed'
+  );
+});
+
+test('aria-owns should not be set initiailly', function (assert) {
+  var $select = $('#qunit-fixture .multiple');
+
+  var CustomSelection = Utils.Decorate(MultipleSelection, InlineSearch);
+  var selection = new CustomSelection($select, options);
+  var $selection = selection.render();
+
+  var container = new MockContainer();
+  selection.bind(container, $('<span></span>'));
+
+  // Update the selection so the search is rendered
+  selection.update([]);
+
+  var $search = $selection.find('input');
+
+  assert.ok(
+    !$search.attr('aria-owns'),
+    'The search box should not point to the results when it is first rendered'
+  );
+});
+
+test('aria-owns should be set when opened', function (assert) {
+  var $select = $('#qunit-fixture .multiple');
+
+  var CustomSelection = Utils.Decorate(MultipleSelection, InlineSearch);
+  var selection = new CustomSelection($select, options);
+  var $selection = selection.render();
+
+  var container = new MockContainer();
+  selection.bind(container, $('<span></span>'));
+
+  // Update the selection so the search is rendered
+  selection.update([]);
+
+  var $search = $selection.find('input');
+
+  container.trigger('open');
+
+  assert.ok(
+    $search.attr('aria-owns'),
+    'The search should point to the results when it is opened'
+  );
+});
+
+test('aria-owns should be removed when closed', function (assert) {
+  var $select = $('#qunit-fixture .multiple');
+
+  var CustomSelection = Utils.Decorate(MultipleSelection, InlineSearch);
+  var selection = new CustomSelection($select, options);
+  var $selection = selection.render();
+
+  var container = new MockContainer();
+  selection.bind(container, $('<span></span>'));
+
+  // Update the selection so the search is rendered
+  selection.update([]);
+
+  var $search = $selection.find('input');
+  $search.attr('aria-owns', 'something');
+
+  container.trigger('close');
+
+  assert.ok(
+    !$search.attr('aria-owns'),
+    'There are no results for the search box to point to when it is closed'
   );
 });
