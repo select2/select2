dc4e83379cb6a3f4316bbac8e3215dd30ae04262
Kevin Brown <kevin@kevin-brown.com>
2019-08-07 21:30:49 -0400

    Recompiled dist for 4.0.9

--- dist/js/i18n/de.js
+++ dist/js/i18n/de.js
@@ -1,3 +1,3 @@
-/*! Select2 4.0.8 | https://github.com/select2/select2/blob/master/LICENSE.md */
+/*! Select2 4.0.9 | https://github.com/select2/select2/blob/master/LICENSE.md */
 
 !function(){if(jQuery&&jQuery.fn&&jQuery.fn.select2&&jQuery.fn.select2.amd)var e=jQuery.fn.select2.amd;e.define("select2/i18n/de",[],function(){return{errorLoading:function(){return"Die Ergebnisse konnten nicht geladen werden."},inputTooLong:function(e){return"Bitte "+(e.input.length-e.maximum)+" Zeichen weniger eingeben"},inputTooShort:function(e){return"Bitte "+(e.minimum-e.input.length)+" Zeichen mehr eingeben"},loadingMore:function(){return"Lade mehr Ergebnisse…"},maximumSelected:function(e){var n="Sie können nur "+e.maximum+" Eintr";return 1===e.maximum?n+="ag":n+="äge",n+=" auswählen"},noResults:function(){return"Keine Übereinstimmungen gefunden"},searching:function(){return"Suche…"},removeAllItems:function(){return"Entferne alle Gegenstände"}}}),e.define,e.require}();
