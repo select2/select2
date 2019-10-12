35011ecfd4b159fca54adcdd9213e77ad124d373
Avni Rexhepi <arexhepi@gmail.com>
2019-10-11 23:17:05 -0600

    Close the dropdown when user taps the background on iOS

--- src/js/select2/dropdown/attachBody.js
+++ src/js/select2/dropdown/attachBody.js
@@ -26,7 +26,7 @@ define([
       self._detachPositioningHandler(container);
     });
 
-    this.$dropdownContainer.on('mousedown', function (evt) {
+    this.$dropdownContainer.on('touchend mousedown', function (evt) {
       evt.stopPropagation();
     });
   };
