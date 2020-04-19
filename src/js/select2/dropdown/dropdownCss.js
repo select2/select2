6d064ca343a812c8e36def4d8051c271b4a31d18
Kevin Brown <kevin@kevin-brown.com>
2020-04-19 12:06:53 -0400

    Clean up unused adapter logic for *CssClass

--- src/js/select2/dropdown/dropdownCss.js
+++ src/js/select2/dropdown/dropdownCss.js
@@ -2,11 +2,6 @@ define([
   'jquery',
   '../utils'
 ], function ($, Utils) {
-  // No-op CSS adapter that discards all classes by default
-  function _dropdownAdapter (clazz) {
-    return null;
-  }
-
   function DropdownCSS () { }
 
   DropdownCSS.prototype.render = function (decorated) {
@@ -14,21 +9,10 @@ define([
 
     var dropdownCssClass = this.options.get('dropdownCssClass') || '';
 
-    var dropdownCssAdapter = _dropdownAdapter;
-
     if (dropdownCssClass.indexOf(':all:') !== -1) {
       dropdownCssClass = dropdownCssClass.replace(':all:', '');
 
-      var _cssAdapter = dropdownCssAdapter;
-
       dropdownCssAdapter = function (clazz) {
-        var adapted = _cssAdapter(clazz);
-
-        if (adapted != null) {
-          // Append the old one along with the adapted one
-          return adapted + ' ' + clazz;
-        }
-
         return clazz;
       };
     }
--- src/js/select2/selection/selectionCss.js
+++ src/js/select2/selection/selectionCss.js
@@ -2,42 +2,26 @@ define([
   'jquery',
   '../utils'
 ], function ($, Utils) {
-  // No-op CSS adapter that discards all classes by default
-  function _selectionAdapter (clazz) {
-    return null;
-  }
-
   function SelectionCSS () { }
 
   SelectionCSS.prototype.render = function (decorated) {
-    var $container = decorated.call(this);
+    var $selection = decorated.call(this);
 
     var selectionCssClass = this.options.get('selectionCssClass') || '';
-
-    var selectionCssAdapter = _selectionAdapter;
 
     if (selectionCssClass.indexOf(':all:') !== -1) {
       selectionCssClass = selectionCssClass.replace(':all:', '');
 
-      var _cssAdapter = selectionCssAdapter;
-
       selectionCssAdapter = function (clazz) {
-        var adapted = _cssAdapter(clazz);
-
-        if (adapted != null) {
-          // Append the old one along with the adapted one
-          return adapted + ' ' + clazz;
-        }
-
         return clazz;
       };
     }
 
-    Utils.syncCssClasses($container, this.$element, selectionCssAdapter);
+    Utils.syncCssClasses($selection, this.$element, selectionCssAdapter);
 
-    $container.addClass(selectionCssClass);
+    $selection.addClass(selectionCssClass);
 
-    return $container;
+    return $selection;
   };
 
   return SelectionCSS;
