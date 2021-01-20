b2be93a6f640f6d5c0b555bc611e4134211703ae
Mesut GÖK <mesutgok@hotmail.com>
2021-01-20 13:59:02 +0300

    Added the highlighted item selected with tab key.

--- src/js/select2/core.js
+++ src/js/select2/core.js
@@ -302,12 +302,12 @@ define([
       var key = evt.which;
 
       if (self.isOpen()) {
-        if (key === KEYS.ESC || key === KEYS.TAB ||
+        if (key === KEYS.ESC ||
             (key === KEYS.UP && evt.altKey)) {
           self.close(evt);
 
           evt.preventDefault();
-        } else if (key === KEYS.ENTER) {
+        } else if (key === KEYS.ENTER || key === KEYS.TAB) {
           self.trigger('results:select', {});
 
           evt.preventDefault();
