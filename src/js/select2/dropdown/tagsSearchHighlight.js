2f8e1bb8ee9a2e972affbbc630febf2040d97f1f
Kevin Brown <kevin@kevin-brown.com>
2021-01-22 10:51:36 -0500

    Fix tabbing in newly added decorator

--- src/js/select2/dropdown/tagsSearchHighlight.js
+++ src/js/select2/dropdown/tagsSearchHighlight.js
@@ -1,31 +1,31 @@
 define([
-    '../utils'
+  '../utils'
 ], function (Utils) {
-    function TagsSearchHighlight () { }
+  function TagsSearchHighlight () { }
 
-    TagsSearchHighlight.prototype.highlightFirstItem = function (decorated) {
-        var $options = this.$results
-        .find(
-            '.select2-results__option--selectable' +
-            ':not(.select2-results__option--selected)'
-        );
+  TagsSearchHighlight.prototype.highlightFirstItem = function (decorated) {
+    var $options = this.$results
+    .find(
+      '.select2-results__option--selectable' +
+      ':not(.select2-results__option--selected)'
+    );
 
-        if ($options.length > 0) {
-            var $firstOption = $options.first();
-            var data = Utils.GetData($firstOption[0], 'data');
-            var firstElement = data.element;
+    if ($options.length > 0) {
+      var $firstOption = $options.first();
+      var data = Utils.GetData($firstOption[0], 'data');
+      var firstElement = data.element;
 
-            if (firstElement && firstElement.getAttribute) {
-                if (firstElement.getAttribute('data-select2-tag') === 'true') {
-                    $firstOption.trigger('mouseenter');
+      if (firstElement && firstElement.getAttribute) {
+        if (firstElement.getAttribute('data-select2-tag') === 'true') {
+          $firstOption.trigger('mouseenter');
 
-                    return;
-                }
-            }
+          return;
         }
+      }
+    }
 
-        decorated.call(this);
-    };
+    decorated.call(this);
+  };
 
-    return TagsSearchHighlight;
+  return TagsSearchHighlight;
 });
