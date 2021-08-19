8300bbdb4cb2a230ff8dd4af9d50fd7774d053ee
sultanov <baxt5878@gmail.com>
2021-08-19 15:16:49 +0200

    Uzbek translation added

---
+++ src/js/select2/i18n/uz.js
@@ -0,0 +1,54 @@
+define(function () {
+    // Uzbek
+    return {
+      errorLoading: function () {
+        return "Natijalarni yuklab bo'lmadi.";
+      },
+      inputTooLong: function (args) {
+        var overChars = args.input.length - args.maximum;
+  
+        var message = 'Iltimos ' + overChars + ' belgi kamroq kiriting';
+  
+        if (overChars != 1) {
+          message += 's';
+        }
+  
+        return message;
+      },
+      inputTooShort: function (args) {
+        var remainingChars = args.minimum - args.input.length;
+  
+        var message = 'Iltimos ' + remainingChars + " belgi ko'proq kiriting";
+  
+        return message;
+      },
+      loadingMore: function () {
+        return 'Natijalar yuklanmoqda…';
+      },
+      maximumSelected: function (args) {
+        var message = 'Siz faqat ' + args.maximum + ' ta element tanlay olasiz';
+  
+        if (args.maximum != 1) {
+          message += 's';
+        }
+  
+        return message;
+      },
+      noResults: function () {
+        return 'Hech qanday natija topilmadi';
+      },
+      searching: function () {
+        return 'Qidirilmoqda…';
+      },
+      removeAllItems: function () {
+        return 'Barcha elementlarni olib tashlang';
+      },
+      removeItem: function () {
+        return 'Elementni olib tashlash';
+      },
+      search: function() {
+        return 'Qidirmoq';
+      }
+    };
+  });
+  
