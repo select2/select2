928c122327467c0e2e031affcc9e223f3eaee6bd
Bahtiyor Sultanov <bahtiyor.sultanov@gtsol.com>
2022-03-10 21:08:16 +0100

    some fixes

--- src/js/select2/i18n/uz.js
+++ src/js/select2/i18n/uz.js
@@ -1,54 +1,49 @@
 define(function () {
     // Uzbek
     return {
-      errorLoading: function () {
-        return "Natijalarni yuklab bo'lmadi.";
-      },
-      inputTooLong: function (args) {
-        var overChars = args.input.length - args.maximum;
-  
-        var message = 'Iltimos ' + overChars + ' belgi kamroq kiriting';
-  
-        if (overChars != 1) {
-          message += 's';
+        errorLoading: function () {
+            return "Natijalarni yuklab bo'lmadi.";
+        },
+        inputTooLong: function (args) {
+            var overChars = args.input.length - args.maximum;
+
+            var message = overChars + ' belgi kamroq kiriting';
+
+            if (overChars != 1) {
+                message += 's';
+            }
+
+            return message;
+        },
+        inputTooShort: function (args) {
+            var remainingChars = args.minimum - args.input.length;
+
+            var message = remainingChars + " belgi ko'proq kiriting";
+
+            return message;
+        },
+        loadingMore: function () {
+            return 'Natijalar yuklanmoqda…';
+        },
+        maximumSelected: function (args) {
+            var message = 'Siz faqat ' + args.maximum + ' ta element tanlay olasiz';
+
+            return message;
+        },
+        noResults: function () {
+            return 'Hech qanday natija topilmadi';
+        },
+        searching: function () {
+            return 'Qidirilmoqda…';
+        },
+        removeAllItems: function () {
+            return 'Barcha elementlarni olib tashlang';
+        },
+        removeItem: function () {
+            return 'Elementni olib tashlash';
+        },
+        search: function () {
+            return 'Qidirmoq';
         }
-  
-        return message;
-      },
-      inputTooShort: function (args) {
-        var remainingChars = args.minimum - args.input.length;
-  
-        var message = 'Iltimos ' + remainingChars + " belgi ko'proq kiriting";
-  
-        return message;
-      },
-      loadingMore: function () {
-        return 'Natijalar yuklanmoqda…';
-      },
-      maximumSelected: function (args) {
-        var message = 'Siz faqat ' + args.maximum + ' ta element tanlay olasiz';
-  
-        if (args.maximum != 1) {
-          message += 's';
-        }
-  
-        return message;
-      },
-      noResults: function () {
-        return 'Hech qanday natija topilmadi';
-      },
-      searching: function () {
-        return 'Qidirilmoqda…';
-      },
-      removeAllItems: function () {
-        return 'Barcha elementlarni olib tashlang';
-      },
-      removeItem: function () {
-        return 'Elementni olib tashlash';
-      },
-      search: function() {
-        return 'Qidirmoq';
-      }
     };
-  });
-  
+});
