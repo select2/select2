bc3fd376e03745e94c7d800ebc15ce0050e3ae24
Petrik <petrik@deheus.net>
2023-02-21 19:13:14 +0100

    Upgrade dev node to v12
    - Upgrade node to v12 for Github actions.
    - Compiled and minified src with Grunt

--- dist/js/select2.js
+++ dist/js/select2.js
@@ -1312,7 +1312,10 @@ S2.define('select2/results',[
             data: data
           });
         } else {
-          self.trigger('close', {});
+          self.trigger('close', {
+            originalEvent: evt,
+            data: data
+          });
         }
 
         return;
@@ -3751,7 +3754,7 @@ S2.define('select2/data/ajax',[
       }, function () {
         // Attempt to detect if a request was aborted
         // Only works if the transport exposes a status property
-        if ('status' in $request &&
+        if ($request && 'status' in $request &&
             ($request.status === 0 || $request.status === '0')) {
           return;
         }
