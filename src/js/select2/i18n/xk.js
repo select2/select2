070bad17403f416522611ce1819c3aeb7007ea3f
Timur Ablialimov <timur.ablialimov@paysera.net>
2023-07-18 13:05:02 +0300

    fix linting

--- src/js/select2/i18n/xk.js
+++ src/js/select2/i18n/xk.js
@@ -7,7 +7,9 @@ define(function () {
         inputTooLong: function (args) {
             var overChars = args.input.length - args.maximum;
 
-            var message = 'Ju lutemi fshini karakterin ' + overChars + ' karaktere';
+            var message = 'Ju lutemi fshini karakterin ' +
+              overChars +
+              ' karaktere';
 
             if (overChars != 1) {
                 message += 't';
@@ -18,7 +20,9 @@ define(function () {
         inputTooShort: function (args) {
             var remainingChars = args.minimum - args.input.length;
 
-            var message = 'Ju lutemi, shkruani ' + remainingChars + ' ose mГ« shumГ« karaktere';
+            var message = 'Ju lutemi, shkruani ' +
+              remainingChars +
+              ' ose mГ« shumГ« karaktere';
 
             return message;
         },
@@ -26,7 +30,8 @@ define(function () {
             return 'Duke ngarkuar më shumë rezultate…';
         },
         maximumSelected: function (args) {
-            var message = 'Mund tГ« zgjidhni vetГ«m artikullin ' + args.maksimum;
+            var message = 'Mund tГ« zgjidhni vetГ«m artikullin ' +
+              args.maksimum;
 
             if (args.maximum != 1) {
                 message += ' artikuj';
