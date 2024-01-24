0479e7e6350a7d6c7e2a2fd26a2e6107d35fed0a
Billy Noah <billynoah@gmail.com>
2024-01-24 16:27:25 -0500

    Update placeholder.js
    Validate placeholder as an object

--- src/js/select2/selection/placeholder.js
+++ src/js/select2/selection/placeholder.js
@@ -8,7 +8,7 @@ define([
   }
 
   Placeholder.prototype.normalizePlaceholder = function (_, placeholder) {
-    if (typeof placeholder === 'string') {
+    if (typeof placeholder !== 'object') {
       placeholder = {
         id: '',
         text: placeholder
